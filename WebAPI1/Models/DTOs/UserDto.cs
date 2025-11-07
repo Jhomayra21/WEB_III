89d06ccbe7e6cb4b3997ba537476450f1697bd3c
Jhomayra21 <jhomayramamanichoqueticlla21@gmail.com>
2025-11-06 20:57:16 -0400

    WEB_API

---
+++ WebAPI1/Models/DTOs/UserDto.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI1.Models.DTOs
+{
+    public class UserDto
+    {
+        [Required, MaxLength(100)]
+        public string UserName { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+    public class RegisterUserDto
+    {
+        [Required, MaxLength(100)]
+        public string UserName { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+    public class UpdateUserDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string? Password { get; set; } 
+    }
+    public class DeleteUserDto
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}
